d21f8fc6279b4e97110fccec002440ddbe2136a9
Arpit bhatia <arpitbhatia@arpits-macbook-air-2.local>
2018-12-24 19:26:39 +0530

    Convert to mvc design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Arpit bhatia on 24/12/18.
+//  Copyright © 2018 Sleeping8. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
